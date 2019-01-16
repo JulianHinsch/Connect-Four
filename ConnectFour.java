84374ba49a8cae7efb7ea23e972fe5e876c33530
Julian Hinsch <jhinsch799@gmail.com>
2019-01-16 14:45:11 -0500

    refactor and clean up

--- ConnectFour.java
+++ ConnectFour.java
@@ -3,55 +3,40 @@ import java.util.ArrayList;
 import processing.core.*;
 
 /**
- * Connect Four Application
- * 
- * Graphic Connect Four game made with Processing library.
+ * Connect Four
  * 
  * User chooses red or black.
  * Plays against simple AI that drops tokens at strategic locations.
  * User has the option to play another game upon completion.
  * 
  * @author Julian Hinsch
- * @version 6/1/17
  */
-public class ConnectFourApp extends PApplet {
-	//font
+public class ConnectFour extends PApplet {
+
 	private PFont f;
-	//user color
 	private String usercolor;
-	//comp color
 	private String compcolor;
-	//game stage
 	private String gamestage;
-	//game board
 	private Board board;
-	//red choice token
 	private Token redtoken;
-	//black choice token
 	private Token blacktoken;
 	
 	/**
-	 * Setup Method
-	 * 
-	 * Initializes and displays the game board, background, and text.
+	 * Initializes the background, text, color choice tokens, and gamestage.
 	 */
 	public void setup() {
 		size(720, 480);
 		background(139,189,255);
 		displayBoardText();
 		noLoop();
-		//make the 2 tokens
-		redtoken = new Token(this,width/2-25,height/2+10,"red",false);
-		blacktoken = new Token(this,width/2+25,height/2+10,"black",false);
-		gamestage="colorchoice";
+		redtoken = new Token(this, width/2-25, height/2+10, "red", false);
+		blacktoken = new Token(this, width/2+25, height/2+10, "black", false);
+		gamestage = "colorchoice";
 		redraw();
 	}
 	
 	/**
-	 * Draw Method
-	 * 
-	 * Contains a switch statement that displays various objects
-	 * and text depending on the current state of the game.
+	 * Displays various objects and text depending on the current game stage.
 	 */
 	public void draw() {
 		switch(gamestage) {
@@ -84,69 +69,55 @@ public class ConnectFourApp extends PApplet {
 	}
 	
 	/**
-	 * Mouse Clicked Method
-	 * 
-	 * Executes different events on a mouse click
-	 * depending on the current stage of the game.
+	 * Executes different code depending on the current game stage.
 	 */
 	public void mouseClicked() {
 		switch(gamestage) {
 			case "colorchoice":
 				if (redtoken.mouseWithin()) {
 					//user chose red
-					usercolor="red";
-					compcolor="black";
-					//create board
+					usercolor = "red";
+					compcolor = "black";
 					board = new Board(this);
 					board.initializeTokenArray();
-					//draw the empty board
 					redraw();
-					//next stage
-					gamestage="userturn";
+					gamestage = "userturn";
 					break;
 				} else if (blacktoken.mouseWithin()) {
 					//user chose black
-					usercolor="black";
-					compcolor="red";
-					//create board
+					usercolor = "black";
+					compcolor = "red";
 					board = new Board(this);
 					board.initializeTokenArray();
-					//draw the empty board
 					redraw();
-					//next stage
-					gamestage="userturn";
-					break;
-					
-				} else {
+					gamestage = "userturn";
 					break;
 				}
+				break;
 			case "userturn": 
-				boolean usermoved=false;
+				boolean usermoved = false;
 				for (int x = 0; x < 7; x++) {
 					for (int y = 0; y < 6; y++) {
-						//compute which token (if any) the mouse is above
+						//determine which token (if any) the mouse is above
 						if (board.tokenarray[x][y].mouseWithin()) {
 							//make sure the column isn't full
 							if (board.tokenarray[x][5].getColor().equals("blue")) {
-								executeMove(x,y,usercolor);
-								usermoved=true;
+								executeMove(x, y, usercolor);
+								usermoved = true;
 								redraw();
 							}
 						}
 					}
 				}
-				if(usermoved==false) {
-					//no move was made
+				if(usermoved == false) {
 					break;
 				} else if (board.testWin(usercolor)) {
-					//a winning move was made
-					gamestage="userwin";
+					gamestage = "userwin";
 					redraw(); 
 					break;
 				} else {
-					//computer gets to move
-					gamestage="compturn";
-					//wait for 1 second
+					gamestage = "compturn";
+					//wait for 1 second - not necessary but better for UX
 					try {
 					    Thread.sleep(1000);
 					} catch(InterruptedException ex){
@@ -156,43 +127,38 @@ public class ConnectFourApp extends PApplet {
 					compMove();
 					redraw();
 					if (board.testWin(compcolor)) {
-						//computer won
-						gamestage="compwin";
+						gamestage = "compwin";
 						redraw();
-						usermoved=false;
+						usermoved = false;
 						break;
-					} else if (board.getNumTokens()==42) {
+					} else if (board.getNumTokens() == 42) {
 						//computer filled the board
-						gamestage="fullboard";
+						gamestage = "fullboard";
 						redraw();
-						usermoved=false;
+						usermoved = false;
 						break;
 					} else {
-						//go back to the user
-						gamestage="userturn";
+						//return control to user
+						gamestage = "userturn";
 						redraw();
-						usermoved=false;
+						usermoved = false;
 						break;	
 					}
 				}
 			case "compturn": 
-				//do nothing
-					break;
+				break;
 			case "userwin":
-				//click anywhere to play again
-				gamestage="colorchoice";
+				gamestage = "colorchoice";
 				board.hide();
 				redraw();
 				break;
 			case "compwin":
-				//click anywhere to play again
-				gamestage="colorchoice";
+				gamestage = "colorchoice";
 				board.hide();
 				redraw();
 				break;
 			case "fullboard":
-				//click anywhere to play again
-				gamestage="colorchoice";
+				gamestage = "colorchoice";
 				board.hide();
 				redraw();
 				break;
@@ -201,34 +167,24 @@ public class ConnectFourApp extends PApplet {
 		}
 	}
 	
-//computer AI
 	
 	/**
-	 * Computer Move Method
-	 * 
-	 * Find the best move to make, and
-	 * drop a token of the opposite color as the user.
+	 * Place a token in the most strategic column.
 	 */
 	private void compMove() {
-		//get a smart move
 		int x = findBestMove();
-		//execute the move
-		executeMove(x,5,compcolor);
+		executeMove(x, 5, compcolor);
 	}
 	
 	/**
-	 * Execute Move Method
+	 * Sets token color at lowest possible row.
 	 * 
-	 * Executes a move at lowest possible Y position.
-	 * 
-	 * Computer moves are already validated by findBestMove()
-	 * User moves may cause IllegalArgumentException if column is full.
-	 * 
-	 * @param the x position of the desired move on the board.
-	 * @param the color to change the space to.
+	 * @param x, the selected column.
+	 * @param maxY, the desired row selected by user (computer always selects top row).
+	 * @param color, the color to change the token to.
 	 */
-	private void executeMove(int x,int maxy,String color){
-		for (int y=0;y<=maxy; y++) {
+	private void executeMove(int x,int maxY,String color){
+		for (int y = 0; y <= maxY; y++) {
 			//iterate up column
 			if (board.tokenarray[x][y].getColor().equals("blue")) {
 				//execute move - only once
@@ -239,121 +195,95 @@ public class ConnectFourApp extends PApplet {
 		}
 	}
 	
+
 	/**
 	 * Find Best Move Method
 	 * 
-	 * Efficiently iterates through possible computer moves.
-	 * 
-	 * There are 7 possible moves, stored in moves[].
+	 * There are never more than 7 possible moves.
 	 * A score is calculated for each move based on its neighbors.
-	 * 
 	 * The move with the best score is returned.
 	 * If there are multiple possible moves with the same score,
 	 * one is chosen randomly.
-	 * 
-	 * @return the column in which to place a token.
 	 */
 	private int findBestMove() {
-		//array of move scores
-		int[] movescores = {0,0,0,0,0,0,0};
-		//iterate through the 7 possible moves
+		int[] moveScores = {0,0,0,0,0,0,0};
 		for (int x = 0; x < 7; x++) {
 			for (int y = 0; y<6; y++) {
-				//make sure its empty
 				if (board.tokenarray[x][y].getColor().equals("blue")) {	
 					//check if this move can be made
-					//move must be on floor or above a previously placed Token
+					//due to 'gravity', move must be on floor or above a previously placed token
 					if (y==0 || !board.tokenarray[x][y-1].getColor().equals("blue")) {
-						//calculate the score
-						movescores[x]=calculateMoveScore(x,y);
-						//dont look at impossible moves above this y position
+						moveScores[x]=calculateMoveScore(x,y);
 						break;
 					}
 				}
 			}
 		}		
-		//iterate through moves[]
+		//iterate over moveScores
 		//find the best score, and store all moves with this score
-		int bestscore = 0;
-		ArrayList<Integer> bestmoves = new ArrayList<Integer>();
+		int bestScore = 0;
+		ArrayList<Integer> bestMoves = new ArrayList<Integer>();
 		//pick the best move in the list
 		for (int x = 0; x < 7; x++) {
-			if (movescores[x]==bestscore) {
-				//this move is equivalent to moves already in the ArrayList
-				bestmoves.add(x);
-			} else if (movescores[x]>bestscore) {
-				//clear the ArrayList and add only this move
-				bestmoves = new ArrayList<Integer>();
-				bestmoves.add(x);
+			if (moveScores[x]==bestScore) {
+				//move is equivalent to moves already in bestMoves
+				bestMoves.add(x);
+			} else if (moveScores[x]>bestScore) {
+				//reinitialize bestMoves with only this move
+				bestMoves = new ArrayList<Integer>();
+				bestMoves.add(x);
 				//this move had the best score so far
-				bestscore=movescores[x];
+				bestScore = moveScores[x];
 			}
 		}
-		//randomly choose a move from the ArrayList
-		int randomindex=(int)(Math.random()*bestmoves.size());
-		//return one of the moves
+		//randomly select a move from bestMoves
+		int randomindex = (int)(Math.random() * bestMoves.size());
 		return bestmoves.get(randomindex);
 	}
 	
+
 	/**
-	 * Calculate Move Score Method
-	 * 
-	 * Calculates a score for this move based on neighboring tokens.
+	 * Calculates a score (0-4) for a possible move based on neighboring tokens.
 	 * 
 	 * @param x, the x position of the move to be considered
 	 * @param y, the y position of the move to be considered
-	 * @return a score, 0-4, for this move.
 	 */
 	private int calculateMoveScore(int x,int y) {		
-		//user neighbors
-		//horizontal
+
 		int hzontaluser=min(3,(countNeighborsDirection(x,y,usercolor,1,0)+countNeighborsDirection(x,y,usercolor,-1,0)));
-		//down
 		int vticaluser=countNeighborsDirection(x,y,usercolor,0,-1);
-		//right diagonal
 		int rdiaguser=min(3,(countNeighborsDirection(x,y,usercolor,1,1)+countNeighborsDirection(x,y,usercolor,-1,-1)));
-		//left diagonal
 		int ldiaguser=min(3,(countNeighborsDirection(x,y,usercolor,-1,1)+countNeighborsDirection(x,y,usercolor,1,-1)));
-		//maximum
 		int maxuserneighbors=max(max(hzontaluser,vticaluser),max(rdiaguser,ldiaguser));
+
+		int hzontalcomp = min(3,(countNeighborsDirection(x,y,compcolor,1,0)+countNeighborsDirection(x,y,compcolor,-1,0)));
+		int vticalcomp = countNeighborsDirection(x,y,compcolor,0,-1);
+		int rdiagcomp = min(3,(countNeighborsDirection(x,y,compcolor,1,1)+countNeighborsDirection(x,y,compcolor,-1,-1)));
+		int ldiagcomp = min(3,(countNeighborsDirection(x,y,compcolor,-1,1)+countNeighborsDirection(x,y,compcolor,1,-1)));
+		int maxcompneighbors = max(max(hzontalcomp,vticalcomp),max(rdiagcomp,ldiagcomp));
 		
-		//comp neighbors
-		//horizontal
-		int hzontalcomp=min(3,(countNeighborsDirection(x,y,compcolor,1,0)+countNeighborsDirection(x,y,compcolor,-1,0)));
-		//down
-		int vticalcomp=countNeighborsDirection(x,y,compcolor,0,-1);
-		//right diagonal
-		int rdiagcomp=min(3,(countNeighborsDirection(x,y,compcolor,1,1)+countNeighborsDirection(x,y,compcolor,-1,-1)));
-		//left diagonal
-		int ldiagcomp=min(3,(countNeighborsDirection(x,y,compcolor,-1,1)+countNeighborsDirection(x,y,compcolor,1,-1)));
-		//maximum
-		int maxcompneighbors=max(max(hzontalcomp,vticalcomp),max(rdiagcomp,ldiagcomp));
-		
-		if (maxcompneighbors==3) {
-			//this is a winning move
+		if (maxcompneighbors == 3) {
+			//this is a winning move for the computer
 			return 4;
 		} else {
 			return max(maxuserneighbors,maxcompneighbors);
 		}
-		
 	}
 	
+
 	/**
-	 * Count Neighbors in Direction Method
-	 * 
-	 * Looks for a column of user tokens near the move to be considered,
+	 * Look for user tokens near the move to be considered,
 	 * in a direction specified by parameters xiter and yiter.
 	 * 
-	 * Returns the length of the uninterrupted row (0,1,2, or 3).
+	 * Returns the number of tokens in that direction (0, 1, 2, or 3).
 	 * 
 	 * @param x, the x position of the move to be considered
 	 * @param y, the y position of the move to be considered
 	 * @param color, the color of the tokens to look for
 	 * @param xiter, the direction to iterate the x-coordinate
 	 * @param yiter, the direction to iterate the y-coordinate
-	 * @return the length of the uninterrupted row (0,1,2,or 3).
 	 */
-	private int countNeighborsDirection(int x,int y, String color, int xiter, int yiter) {
+	private int countNeighborsDirection(int x, int y, String color, int xiter, int yiter) {
 		int count=0;
 		for (int i = 1; i <= 3; i++) {
 			//x is out of bounds
@@ -371,102 +301,62 @@ public class ConnectFourApp extends PApplet {
 		return count;
 	}	
 	
-//methods to display text
-	
 	/**
-	 * Board Full Method
-	 * 
-	 * Displays text when board is full.
+	 * Displays "Board is full!", "Click anywhere to play again."
 	 */
 	private void displayBoardFullText() {
-		//set font color
 		fill(255);
-		//align center
 		textAlign(CENTER);
-		//initialize font
 		f = createFont("Helvetica-Bold",20,true);
-		//set font
 		textFont(f);
-		//display text
 		text("Board is full!",width/2,height/2+180);
 		text("Click anywhere to play again.",width/2,height/2+200);
 	}
 	
 	/**
-	 * Display Board Text Method
-	 * 
-	 * Displays "Connect Four"
-	 * Displays "Copyright Julian Hinsch 2017"
+	 * Displays "Connect Four", "Copyright Julian Hinsch 2017"
 	 */
 	private void displayBoardText(){
-		//set font color
 		fill(255);
-		//align center
 		textAlign(CENTER);
-		//title text
-		//initialize font
 		f = createFont("Helvetica-Bold",48,true);
-		//set font
 		textFont(f);
-		//display text
 		text("Connect Four",width/2,65);
-		//copyright text
-		//initialize font
 		f = createFont("Helvetica-Bold",15,true);
-		//set font
 		textFont(f);
-		//display text
 		text("\u00a9 2017 Julian Hinsch",width/2,470);
 	}
 	
 	/**
-	 * Display Choice Text Method
-	 * 
-	 * Displays"Choose black or red:"
+	 * Displays "Choose black or red: "
 	 */
 	private void displayChoiceText() {
-		//set font color
 		fill(255);
-		//align center
 		textAlign(CENTER);
-		//initialize font
 		f = createFont("Helvetica-Bold",24,true);
-		//set font
 		textFont(f);
 		text("Choose red or black:",width/2,height/2-30);
 	}
 	
 	/**
-	 * Display Computer Win Text Method
-	 * 
-	 * Displays text after computer wins.
+	 * Displays "You lost!", "Click anywhere to play again."
 	 */
 	private void displayCompWinText() {
-		//set font color
 		fill(255);
-		//align center
 		textAlign(CENTER);
-		//initialize font
 		f = createFont("Helvetica-Bold",20,true);
-		//set font
 		textFont(f);
 		text("You lost!",width/2,height/2+180);
 		text("Click anywhere to play again.",width/2,height/2+200);
 	}
 	
 	/**
-	 * Display User Win Text Method
-	 * 
-	 * Displays text after user wins.
+	 * Displays "You won!", "Click anywhere to play again."
 	 */
 	private void displayUserWinText() {
-		//set font color
 		fill(255);
-		//align center
 		textAlign(CENTER);
-		//initialize font
 		f = createFont("Helvetica-Bold",20,true);
-		//set font
 		textFont(f);
 		text("You won!",width/2,height/2+180);
 		text("Click anywhere to play again.",width/2,height/2+200);
--- README.md
+++ README.md
@@ -1,5 +1,24 @@
-# Connect-Four
-Graphic Connect Four game with AI coded in Java using the Processing development environment.
+# Connect Four
+
+Built with Java and the Processing library.
 
 Note: To run this game, you need to first download [Processing](https://github.com/processing/processing)
 
+# How does the 'computer player' work?
+
+There are no more than 7 possible moves at any time in Connect Four.
+To win, the player must line up 4 tokens in any direction. 
+When choosing a move, the computer player aims to either:
+
+- Place tokens in a continuous line
+- Prevent the user from placing tokens in a continuous line
+
+So, at every chance, the computer player assigns a score to each of its possible moves.
+This score is based on the neighboring tokens.  
+
+Some examples of moves evaluated with this scoring system:
+
+- 3 neighboring computer tokens in a line: This move is a winner - score: 4.
+- 3 neighboring user tokens in a line: This move is necessary to prevent the user from winning - but not as good as the above - score: 3.
+
+If there are multiple moves with the best possible score, one is chosen at random.
